360b817c80185e26c9af58522c4c7854b6722803
◄hollsteinm► <hollsteinm@users.noreply.github.com>
2020-11-21 09:42:18 -0600

    Update build-and-publish-ue4-minimal-docker-image.ps1
    Refresh environment for new python path.

--- build-and-publish-ue4-minimal-docker-image.ps1
+++ build-and-publish-ue4-minimal-docker-image.ps1
@@ -20,6 +20,7 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
 	'https://chocolatey.org/install.ps1'
 ))
 choco install -y python
+refreshenv
 
 # Setup ue4 docker
 pip install ue4-docker
