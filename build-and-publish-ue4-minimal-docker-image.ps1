cd546d7625bab60bcfad7e2e0078503adff7309c
Martin Hollstein <Hollsteinm@hotmail.com>
2020-11-23 10:13:05 -0600

    Update the build script to set the max size of repos in docker to almost full storage size, docker commit was failing. Also better formatting of the repo names

--- build-and-publish-ue4-minimal-docker-image.ps1
+++ build-and-publish-ue4-minimal-docker-image.ps1
@@ -13,6 +13,21 @@ trap
   exit 1
 }
 
+Stop-Service *docker*
+
+$storageOpts = ,'size=550GB'
+
+$dockerConfig = Get-Content -Path C:\ProgramData\Docker\config\daemon.json -Raw | ConvertFrom-Json
+$dockerConfig
+if($dockerConfig.'storage-opts' -eq $nul) {
+  Add-Member -InputObject $dockerConfig -Name 'storage-opts' -MemberType 'NoteProperty' -Value $storageOpts
+} else {
+  $dockerConfig.'storage-opts' = $storageOpts
+}
+$dockerConfig | ConvertTo-Json -depth 100 | Out-File C:\ProgramData\Docker\config\daemon.json
+
+Start-Service *docker*
+
 # Install Python, Required for ue4-docker
 Write-Output "Installing Python 3 latest"
 Set-ExecutionPolicy Bypass -Scope Process -Force;
@@ -24,9 +39,6 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
 Write-Output "Authenticate with ECR..."
 Invoke-Expression -Command (Get-ECRLoginCommand).Command
 
-# Set Confiugrations
-Set-Variable UE4DOCKER_TAG_NAMESPACE=ue4
-
 choco install -y python --version=3.8.0
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -36,8 +48,22 @@ pip --version
 pip install ue4-docker
 ue4-docker setup
 ue4-docker build $engineVersion --no-engine --exclude debug --exclude templates -username $ue4GitUsername -password $ue4GitPersonalAccessToken
+
+#Assume Windows Server 2019
+$prereqRepoName = "${repositoryName}ue4/ue4-build-prerequisites:ltsc2019"
+$minimalRepoName = "${repositoryName}ue4/ue4-minimal:${engineVersion}-ltsc2019"
+$fullRepoName = "${repositoryName}ue4/ue4-full:${engineVersion}-ltsc2019"
+$currentMinimalRepoTag = "adamrehn/ue4-minimal:${engineVersion}-ltsc2019"
+$currentFullRepoTag = "adamrehn/ue4-full:${engineVersion}-ltsc2019"
+
+docker tag adamrehn/ue4-build-prerequisites:ltsc2019 $prereqRepoName
+docker tag $currentMinimalRepoTag $minimalRepoName
+docker tag $currentFullRepoTag $fullRepoName
+
 ue4-docker clean --source
-docker tag ue4/ue4-full:$engineVersion $repositoryName
-docker push $repositoryName
+
+docker push $prereqRepoName
+docker push $minimalRepoName
+docker push $fullRepoName
 
 return $LASTEXITCODE
